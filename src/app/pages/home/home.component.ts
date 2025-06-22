8ff3c60f971c23bdc8e609715505f6e0d6732927
mouss <moussa.diakitevh@gmail.com>
2025-06-22 18:07:03 +0200

    v-1 portfolio

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,16 +2,9 @@ import { Component } from '@angular/core';
 import { HeroComponent } from './components/hero/hero.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectPreviewComponent } from './components/project-preview/project-preview.component';
-import { ContactComponent } from './components/contact/contact.component';
-
 @Component({
   selector: 'app-home',
-  imports: [
-    HeroComponent,
-    SkillsComponent,
-    ProjectPreviewComponent,
-    ContactComponent,
-  ],
+  imports: [HeroComponent, SkillsComponent, ProjectPreviewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
